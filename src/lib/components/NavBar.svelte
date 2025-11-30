0fe2054a9f41066caed155e0c67d5c7ff28d49f3
Alí Sinisterra <haidarconh@gmail.com>
2025-11-30 16:44:40 -0500

    create NavBar

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,25 @@
+<nav>
+    <div class="section primary">
+        <p>Tinge</p>
+    </div>
+    <button class="section"> Options </button>
+</nav>
+
+<style>
+    button {
+        all: unset;
+    }
+
+    .section {
+        display: flex;
+        flex-shrink: 2;
+        padding: 0.8ch;
+
+        font-weight: 600;
+        border-radius: 100;
+    }
+    .section.primary {
+        flex-grow: 1;
+        flex-shrink: 0;
+    }
+</style>
