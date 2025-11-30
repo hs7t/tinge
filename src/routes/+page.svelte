a41f9b85c4bdc02cdce7d3d79bbc1f5a69317ee6
Haidar Alí Sinisterra Rojas <125828096+hs7t@users.noreply.github.com>
2025-11-30 13:25:44 -0500

    Revert "Work on basic UI"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,37 @@
 <script lang="ts">
-    import PinButton from '$lib/components/buttons/PinButton.svelte'
-
     class RGBColor {
-        red: number
-        green: number
-        blue: number
+      red: number
+      green: number
+      blue: number
 
-        constructor(red: number, green: number, blue: number) {
-            this.red = red
-            this.green = green
-            this.blue = blue
-        }
-
-        toCSSRGBNotation = () => {
-            return `rgb(${this.red}, ${this.green}, ${this.blue})`
-        }
+      constructor (red: number, green: number, blue: number) {
+        this.red = red
+        this.green = green
+        this.blue = blue
+      }
     }
 
     type ColorEntry = {
-        name: string
-        rgbColor: RGBColor
-        id: number
+      name: string,
+      rgbColor: RGBColor,
+      id: number
     }
 
     let colours: Array<ColorEntry> = [
-        {
-            name: 'Green',
-            rgbColor: new RGBColor(0, 255, 0),
-            id: 1,
-        },
+      {
+        name: "Green",
+        rgbColor: new RGBColor(255, 255, 255),
+        id: 1
+      },
     ]
 </script>
 
 <h1>tinge <small><em>/tĭnj/</em></small></h1>
 
 <p>hiya here's some colours</p>
-{#each colours as colour (colour.id)}
-    <div
-        class="colour-block"
-        style:background-color={colour.rgbColor.toCSSRGBNotation()}
-    >
+
+<div>
+    {#each colours as colour (colour.id)}
         {colour.name}
-    </div>
-    <PinButton></PinButton>
-{/each}
+    {/each}
+</div>
