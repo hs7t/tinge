ae79dd674f8a184ae96141fdef2f816ddc9032af
Alí Sinisterra <haidarconh@gmail.com>
2025-11-30 17:10:42 -0500

    create Swatch and SwatchView

---
+++ src/lib/components/Swatch.svelte
@@ -0,0 +1,24 @@
+<script module lang="ts">
+    export type SwatchData = {
+        name: string
+        colour: chroma.Color
+    }
+</script>
+
+<script lang="ts">
+    import PinButton from './buttons/PinButton.svelte'
+
+    let { swatchData }: { swatchData: SwatchData } = $props()
+</script>
+
+<div class="swatch" style:background-color={swatchData.colour.hex()}>
+    <div class="info">
+        <span class="text name">
+            {swatchData.name}
+        </span>
+        <span class="text reference">
+            {swatchData.colour.hex()}
+        </span>
+    </div>
+    <PinButton />
+</div>
---
+++ src/lib/components/SwatchView.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import chroma from 'chroma-js'
+
+    import type { SwatchData } from './Swatch.svelte'
+    import Swatch from './Swatch.svelte'
+
+    let swatches: Array<SwatchData> = [
+        {
+            name: 'Green',
+            colour: chroma('green'),
+        },
+    ]
+</script>
+
+{#each swatches as swatchData, id (id)}
+    <Swatch {swatchData} />
+{/each}
