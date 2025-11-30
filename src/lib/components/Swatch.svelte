fc78fe7abd691ee7790767a64bfe037027998608
Alí Sinisterra <haidarconh@gmail.com>
2025-11-30 18:18:22 -0500

    style Swatch way better

--- src/lib/components/Swatch.svelte
+++ src/lib/components/Swatch.svelte
@@ -6,12 +6,46 @@
 </script>
 
 <script lang="ts">
+    console.log('here!')
+
+    import chroma from 'chroma-js'
+    import { chooseBestContrastingForColour } from '$lib/logic/utilities'
+
     import PinButton from './buttons/PinButton.svelte'
 
     let { swatchData }: { swatchData: SwatchData } = $props()
+
+    let element: HTMLElement | undefined = undefined
+
+    const getVariableValue = (value: string): string => {
+        const fallback = '#1e1e1e'
+        if (!element) return fallback
+
+        let found = window.getComputedStyle(element).getPropertyValue(value)
+        const result: string = found ? found : fallback
+
+        console.log(value, found, fallback)
+        return result
+    }
+
+    let contrastingColor = $state('#000000')
+
+    $effect(() => {
+        if (!element) return
+        contrastingColor = chooseBestContrastingForColour(
+            swatchData.colour,
+            chroma(getVariableValue('--t-color-contrast-A')),
+            chroma(getVariableValue('--t-color-contrast-B')),
+        ).css('rgb')
+    })
 </script>
 
-<div class="swatch" style:background-color={swatchData.colour.hex()}>
+<div
+    class="swatch"
+    bind:this={element}
+    style:background-color={swatchData.colour.hex()}
+    style:color={contrastingColor}
+>
     <div class="info">
         <span class="text name">
             {swatchData.name}
@@ -22,3 +56,23 @@
     </div>
     <PinButton />
 </div>
+
+<style>
+    .swatch {
+        border-radius: var(--t-border-radius-A);
+        padding: 2ch;
+
+        color: var(--color-best-contrasting);
+        background-color: var(--swatch-color);
+
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .swatch .info {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
