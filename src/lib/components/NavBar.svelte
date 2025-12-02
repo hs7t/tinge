6bf354863efae7979248cb09199bc344b1f52702
Alí Sinisterra <haidarconh@gmail.com>
2025-12-01 19:53:36 -0500

    add 'Copy all' button to NavBar

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,8 +1,25 @@
+<script>
+    import { currentState } from '$lib/shared.svelte'
+</script>
+
 <nav>
     <div class="section primary">
         <p>Tinge</p>
     </div>
-    <button class="section"> Options </button>
+    <button class="section" disabled inert title="Coming soon, hopefully">
+        Settings
+    </button>
+    <button
+        class="action"
+        onclick={() => {
+            let colourHEXCodes = currentState.palette.map((swatch) => {
+                return swatch.colour.hex()
+            })
+            navigator.clipboard.writeText(colourHEXCodes.join(', '))
+        }}
+    >
+        Copy all
+    </button>
 </nav>
 
 <style>
@@ -12,7 +29,8 @@
         align-items: center;
     }
 
-    .section {
+    .section,
+    .action {
         --color-best-contrasting: var(--t-color-theme-A-contrast);
 
         display: flex;
@@ -25,18 +43,29 @@
         color: var(--color-best-contrasting);
 
         text-transform: uppercase;
+
+        transition: all 100ms;
     }
 
     .primary.section {
         flex: 1;
     }
 
-    button.section {
+    button.section,
+    button.action {
         --color-best-contrasting: var(--t-color-theme-B-contrast);
 
         background-color: var(--t-color-theme-B);
         color: var(--color-best-contrasting);
 
         outline-offset: 0;
+    }
+
+    button:active {
+        transform: scale(1.09) rotate(5deg);
+    }
+
+    button:disabled {
+        opacity: 0.5;
     }
 </style>
